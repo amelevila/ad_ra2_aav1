cbe7abc49fc15663de1590b3a7a00bbbbe663b59
adria mele <amelevila78@gmail.com>
2025-10-13 16:05:11 +0200

    Projecte mogut

---
+++ src/main/java/com/ra2/Aav1/Ra2Aav1Application.java
@@ -0,0 +1,13 @@
+package com.ra2.Aav1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ra2Aav1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ra2Aav1Application.class, args);
+	}
+
+}
