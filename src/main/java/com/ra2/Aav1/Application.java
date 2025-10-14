ef5661602b295e5565543ab6c39ea605e4e48880
adria mele <amelevila78@gmail.com>
2025-10-14 18:31:41 +0200

    arreglos

--- src/main/java/com/ra2/Aav1/Application.java
+++ src/main/java/com/ra2/Aav1/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Ra2Aav1Application {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Ra2Aav1Application.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
