32b9496a2828eec9791bb38895f50cc1d1334aad
adria mele <amelevila78@gmail.com>
2025-10-15 18:31:14 +0200

    canviats noms dels endpoints

--- src/main/java/com/ra2/Aav1/controller/CustomerController.java
+++ src/main/java/com/ra2/Aav1/controller/CustomerController.java
@@ -18,14 +18,14 @@ public class CustomerController {
 	@Autowired
 	private CustomerRepository customerRep;
 	
-	@PostMapping("/initDB")
+	@PostMapping("/createDB")
 	public String create_insertDB() {
 		customerRep.createTableCustomers();
 		customerRep.insertSampleData();
 		return "taula creada correctament i dades inserides";
 	}
 	
-	@GetMapping("/findAllCustomers")
+	@GetMapping("/customers")
 	public List<Customer> getAllCustomers() {
 		return customerRep.findAll();
 	}
